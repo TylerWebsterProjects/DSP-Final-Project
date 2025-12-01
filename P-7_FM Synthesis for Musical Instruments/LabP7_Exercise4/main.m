e3893125a2b6828703f30a33584d1eda51463a21
sijking <sierra.king2419@gmail.com>
2025-12-01 09:02:41 -0700

    Finished 4.3
    feel free to add analysis as desired

--- P-7_FM Synthesis for Musical Instruments/LabP7_Exercise4/main.m
+++ P-7_FM Synthesis for Musical Instruments/LabP7_Exercise4/main.m
@@ -73,13 +73,90 @@ pause;
 soundsc(150*cos(2*pi*fund_5.*(0:(1/11025):5)), 11025) %FIXIT this was working on my version and it's SO quiet here?? I'm confused
 pause;
 
-%% 4.3c) -> Case 1:
+%% 4.3c) -> Case 1
 % I(t) is the modulation index envelope, and it has the same form as A(t)
 % except that the amplitude is determined by Io. When you increase Io, the
 % "ringing" quality of the bell sound is intensified. At extremes, it makes
 % the sound tinny and clashing. The close Io is to zero, the closer the
 % sound is to a fully synthesized sound.
 
+% f_i(t) is the instantaneous frequency of our sound, and is represented by
+% [fc - I(t)*fm*sin(2*pi*fm*t + phi_m)+ dI/dt*cos(2*pi*fm*t + phi_m)]
+% Note that phi_m/phi_c are arbitrary constants usually chosen to be -pi/2
+
+% I(t) is our bellenv function multiplied by Io, so dI/dt = -Io/tau * exp(-t/tau)
+
+fc = 110; fm = 220; Io = 10; tau = 2; dur = 6; fs = 11025;
+t = 0:(1/fs):dur; %duration 6 seconds at 11025Hz
+It = Io.*bellenv(tau,dur,fs);
+fi = fc - It.*fm.*sin(2*pi*fm.*t - pi/2) - (Io/tau).*exp(-t./tau).*cos(2*pi*fm.*t - pi/2);
+figure(3)
+subplot(1,2,1); plot(t, fi); xlabel("Time"); ylabel("f_{i}(t)"); title("Case 1")
+
+%% 4.3c) -> Case 5
+% same process.
+fc = 250; fm = 350220; Io = 5; tau = 2; dur = 5; fs = 11025;
+t = 0:(1/fs):dur; %duration 6 seconds at 11025Hz
+It = Io.*bellenv(tau,dur,fs);
+fi = fc - It.*fm.*sin(2*pi*fm.*t - pi/2) - (Io/tau).*exp(-t./tau).*cos(2*pi*fm.*t - pi/2);
+
+subplot(1,2,2); plot(t, fi); xlabel("Time"); ylabel("f_{i}(t)"); title("Case 5")
+sgtitle("Instantaneous Frequency (Cases 1 and 5)")
+
+%% 4.3d) -> Case 1 Spectrogram
+% We already calculated the fundamental frequency (by the process given in
+% the lab) to be 110, but from the spectrogram you can see that the
+% frequencies are strongest at the start and fall off over time, just like
+% the instantaneous frequency. See case 5 for comparisons
+figure(4)
+subplot(1,2,1); spectrogram(bell([110,220], 10, 2, 6, 11025)); xlabel("Time"); ylabel("Frequency"); ylim([3675,5*10^4]); colorbar; c = colorbar; c.Label.String = 'Power/Amplitude (dB/Hz)'; clim([-80,-30]);title("Case 1")
+
+%% 4.3d) -> Case 5 Spectrogram
+% We already calculated the fundamental frequency (by the process given in
+% the lab) to be 50, but from the spectrogram you can see that it's
+% stronger at the start and drops off slightly slower. However, you can see
+% a lot more harmonic levels rippling out at the frequencies fm and fc.
+% You can see that around the fundamental frequencies the tones are the
+% strongest, which holds with our idea that it would "sound" right to
+% compare the fundamental frequency to the overall sound.
+subplot(1,2,2); spectrogram(bell([250,350], 5, 2, 5, 11025)); xlabel("Time"); ylabel("Frequency");ylim([3063,5*10^4]); colorbar; c = colorbar; c.Label.String = 'Power/Amplitude (dB/Hz)'; clim([-80,-30]);title("Case 5")
+sgtitle("Bell Spectrograms (Cases 1 and 5)")
+
+%% 4.3e) -> Case 1
+% This point is actually just a comparison of what we graphed in 4.1 and
+% 4.2, so in the code we'll just refer to figures 1 and 2 to talk about
+% this. For case 1, you can see in Figure 1 that the line represented there
+% essentially covers the outer limit of the graph in figure 2. That tracks
+% even just by the name, since an envelope is something that contains
+% another piece of information/etc. 
+
+%% 4.3e) -> Case 5
+% The analysis here is essentially identical to case 1, just on another
+% example. The envelope function in figure 1 represents the outer limit of
+% the sound in figure 2. When zoomed in far, you can see that the bell
+% sound is a strange combination of sinusoids, but that they're all
+% contained within the envelope, which accurately represents the harmonics
+% and other components contained in the bell sound beyond the fundamental
+% frequency.
+
+%% 4.3f) -> Case 1
+% Here we will graph about 200 samples in the middle of the signal and
+% discuss. I mentioned this slightly in 4.3e) Case 5, but we'll get to see
+% this compared directly here.
+% It's pretty easy to see that the fundamental frequency of this signal is
+% one of the limits fc/fm, because all the oscillations are contained
+% within another larger oscillation. This gives the result that it looks
+% almost like a square wave with ripples all across the upper/lower limits.
+figure(5); start = 4*10^4; end_p = start+200;
+subplot(1,2,1); plot(bell([110,220],10,2,6,11025)); title("Case 1"); xlabel("Time"); ylabel("Magnitude"); xlim([start,end_p])
+
+%% 4.3f) -> Case 5
+% This is a case where you can clearly see that the fundamental frequency
+% is not fc/fm, because unlike case 1, it doesn't contain the ripples
+% within a larger oscillation, which leads to strange detours of the signal
+% across the oscillation at unusal points. 
+subplot(1,2,2); plot(bell([250, 350],5,2,5,11025)); title("Case 5"); xlabel("Time"); ylabel("Magnitude");xlim([start,end_p])
+sgtitle("Bell Sound Graph--Close Up (Cases 1 and 5)")
 
 %% 4.4) Comments about the bell
 
